b142c62492886bc4ec1e8f7e9639d0f24b091d55
Thang Man <manvanthang@gmail.com>
2026-02-19 00:56:11 +0700

    Refactor student views and profile update view

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
+from . import student_views
 
 app_name = "courses"
 
 urlpatterns = [
     path("teacher/", views.teacher_home, name="teacher_home"),
     path("student/", views.student_home, name="student_home"),
+    path("profile/update/", student_views.profile_update, name="profile_update"),
     path("create/", views.course_create, name="course_create"),
     path("<int:course_id>/", views.course_detail, name="detail"),
     path("<int:course_id>/edit/", views.course_edit, name="update_course_info"),
